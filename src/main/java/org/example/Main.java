874588aa0069a3859c3fbccb1c54158c7c0d77e8
dinesh.saidapure <dsaidapure07@gmail.com>
2022-06-06 18:33:36 +0530

    Final Commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("It's Dinesh Here");
+        System.out.println("Trials");
     }
 }
